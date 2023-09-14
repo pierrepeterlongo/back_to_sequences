956d6cf74727cebeb8b0da71fa85a6fc2ca20b43
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2023-09-14 16:09:39 +0200

    clean useless files

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# get the full path of the current directory
-install_bin_path=`pwd`
-
-# install kmindex
-cmd="cd ./thirdparty/kmindex/ && sh install.sh -p ${install_bin_path}"
-echo $cmd
-eval $cmd
