0022e11b438db302471a71c09579f24a10da1757
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2023-10-27 14:59:59 +0200

    benchs up to 200 millions reads

--- benchs/benchs.sh
+++ benchs/benchs.sh
@@ -2,7 +2,7 @@ export TIME="\t%E real,\t%U user,\t%S sys,\t%K amem,\t%M mmem"
 
 # Run the benchs
 # Each time check the time to query them
-for nb_reads in 10000 100000 1000000 10000000 100000000
+for nb_reads in 10000 100000 1000000 10000000 100000000 200000000
 do
     echo "nb_reads: $nb_reads"
     time back_to_sequences --in-kmers compacted_kmers.fasta --in-sequences reads_${nb_reads}.fasta  --out-sequences filtered_reads_${nb_reads}.fasta  -k 31 --out-kmers counted_kmers_${nb_reads}.txt
