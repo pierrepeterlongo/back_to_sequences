9db8c65c4b7015c87011352e27ff79ae8acdc2bf
Pierre Peterlongo <pierre.peterlongo@inria.fr>
2025-10-15 13:44:20 +0200

    blast logan

--- logan_blast/README.md
+++ logan_blast/README.md
@@ -1,15 +1,22 @@
-## requires
-pip3 install biopython
-need blast (eg brew install blast)
+The `blast_logan_contigs.sh` script considers a query, and a list of contig files (in contigs.txt). This list may be provided by a logan search result.
 
-## Process to be extended
-Given a contig or unitig list: contigs.txt and a query Q.fa
+For each contig file, it will run local blast between the query and the subset of contigs containing at least one shared k-mer (k=17 by default) with the query (uses `back_to_sequences`), and save the results in a directory named `<query_name>_vs_<accessions>.recruited_contigs`.
+
+## requires 
+- biopython: `pip3 install biopython`
+- blast (*eg* brew install blast or look at https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/)
+- back_to_sequences (cf parent directory :) )
+
+## Running the script
 
 ```bash
-while read accession; do
-	aws s3 cp s3://logan-pub/c/${accession}/${accession}.contigs.fa.zst . --no-sign-request
-	back_to_sequences --in-kmers Q.fa --in-sequences  ${accession}.contigs.fa.zst --out-sequences T.fa
-	python local_blast.py 
-done < contigs.txt
+./blast_logan_contigs.sh --contigs contigs.txt --query query.fa [--delete] [--kmer-size]
 ```
+The `--delete` option will remove the intermediate files (recruited contigs and contigs files) after processing.
 
+## Creating contigs.txt
+Logan-Search results can be used to create contigs.txt. Given a logan search result link:
+
+```bash
+tail -n +2 kmviz-c21feaeb-4f33-4abc-b119-7db7bd47069b/query.tsv | awk '{print $1}' > contigs.txt
+```
--- logan_blast/local_blast.py
+++ logan_blast/local_blast.py
@@ -1,6 +1,10 @@
-from Bio.Blast.Applications import NcbimakeblastdbCommandline, NcbiblastnCommandline
+import subprocess
 from Bio import SeqIO
 import os
+import argparse
+import shutil
+
+
 
 def run_blast_with_alignments(query_fasta, targets_fasta, output_dir="blast_results"):
     os.makedirs(output_dir, exist_ok=True)
@@ -10,31 +14,57 @@ def run_blast_with_alignments(query_fasta, targets_fasta, output_dir="blast_resu
     query_id = query_record.id
 
     # Create a BLAST database from the targets
-    makeblastdb_cline = NcbimakeblastdbCommandline(
-        dbtype="nucl",
-        input_file=targets_fasta,
-        out=os.path.join(output_dir, "targets_db")
-    )
-    makeblastdb_cline()
+    makeblastdb_cmd = [
+        "makeblastdb",
+        "-dbtype", "nucl",
+        "-in", targets_fasta,
+        "-out", os.path.join(output_dir, "targets_db")
+    ]
+    result = subprocess.run(makeblastdb_cmd, check=True, capture_output=True, text=True)
+
+    # Optionally, print or log the output/errors
+    # if result.stdout:
+    #     print("STDOUT:", result.stdout)
+    # if result.stderr:
+    #     print("STDERR:", result.stderr)
 
     # Run BLAST for each target and save alignments
     for target_record in SeqIO.parse(targets_fasta, "fasta"):
         target_id = target_record.id
         output_file = os.path.join(output_dir, f"{query_id}_vs_{target_id}.txt")
 
-        blastn_cline = NcbiblastnCommandline(
-            query=query_fasta,
-            db=os.path.join(output_dir, "targets_db"),
-            out=output_file,
-            outfmt=0,  # Pairwise alignment format
-            num_alignments=1
-        )
-        blastn_cline()
+        # Define your command as a list of arguments
+        blastn_cmd = [
+            "blastn",
+            "-query", query_fasta,
+            "-db", os.path.join(output_dir, "targets_db"),
+            "-out", output_file,
+            "-outfmt", "0",  # Pairwise alignment format
+            "-num_alignments", "1",
+            "-sorthits", "0",
+        ]
 
-        # Print the alignment results
-        with open(output_file, "r") as f:
-            alignment_results = f.read()
-            print(f"Alignment for {query_id} vs {target_id}:\n{alignment_results}\n")
+        # Run the command
+        result = subprocess.run(blastn_cmd, check=True, capture_output=True, text=True)
+
+        # Optionally, print or log the output/errors
+        # if result.stdout:
+        #     print("STDOUT:", result.stdout)
+        # if result.stderr:
+        #     print("STDERR:", result.stderr)
+
+       
 
 if __name__ == "__main__":
-    run_blast_with_alignments("Q.fa", "T.fa")
+    parser = argparse.ArgumentParser(description="Run BLAST alignments between query and target FASTA files.")
+    parser.add_argument("-q", "--query", required=True, help="Query FASTA file")
+    parser.add_argument("-t", "--target", required=True, help="Target FASTA file")
+    args = parser.parse_args()
+    
+    if shutil.which("makeblastdb") is None or shutil.which("blastn") is None:
+        raise EnvironmentError("BLAST+ executables 'makeblastdb' and/or 'blastn' not found in PATH. Please install BLAST+ locally.")
+    
+    query_basename = os.path.splitext(os.path.basename(args.query))[0]
+    target_basename = os.path.splitext(os.path.basename(args.target))[0]
+    run_blast_with_alignments(args.query, args.target, f"{query_basename}_vs_{target_basename}")
+    print(f"\033[92mResults are in directory {query_basename}_vs_{target_basename}\033[0m")
